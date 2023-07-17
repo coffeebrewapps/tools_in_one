8da0e0808804c943f3f0d9df55583c78e2ee2695
Coffee Brew Apps <coffeebrewapps@gmail.com>
2023-07-17 15:52:18 +0800

    Color scheme

--- client/src/views/AboutView.vue
+++ client/src/views/AboutView.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <div class="success">This is a good news.</div>
+    <div class="warning">This is a warning.</div>
+    <div class="error">This is an error.</div>
   </div>
 </template>
 
@@ -9,7 +12,9 @@
   .about {
     min-height: 100vh;
     display: flex;
-    align-items: center;
+    flex-direction: column;
+    place-items: center;
+    place-content: center;
   }
 }
 </style>
--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -3,11 +3,22 @@
 
 <template>
   <main>
-    <h1>This home page</h1>
+    <div class="bordered">
+      <h1>This home page</h1>
+      <p>Hello world!</p>
+    </div>
   </main>
 </template>
 
 <style scoped>
+.bordered {
+  border: 1px solid var(--color-border);
+}
+
+.bordered:hover {
+  border: 1px solid var(--color-border-hover);
+}
+
 @media (min-width: 1024px) {
   main {
     min-height: 100vh;
