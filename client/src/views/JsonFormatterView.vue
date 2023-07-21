c9490c996dad2e67f9227a55282d4917fa7971b4
Coffee Brew Apps <coffeebrewapps@gmail.com>
2023-07-21 10:35:32 +0800

    Json formatter window

--- client/src/views/JsonFormatterView.vue
+++ client/src/views/JsonFormatterView.vue
@@ -1,7 +1,8 @@
 <script setup>
-import { ref, computed } from 'vue';
+import { onMounted, ref, computed } from 'vue';
 import ToolWindow from '@/components/ToolWindow.vue';
 import { useWindowsStore } from '@/stores/windows.js';
+import Prism from 'prismjs';
 
 const windowsStore = useWindowsStore();
 
@@ -16,25 +17,48 @@ function activated() {
   emit('activated', jsonWindow.value);
 }
 
-const jsonData = ref([
-  jsonWindow.value,
-]);
+const spacing = ref(4);
+const rawJson = ref();
 
-const rawJson = computed({
-  get: () => {
-    return JSON.stringify(jsonData.value);
-  },
-  set: (val) => {
-    jsonData.value = JSON.parse(val);
-  },
+const jsonData = computed(() => {
+  if (rawJson.value) {
+    return JSON.parse(rawJson.value);
+  } else {
+    return;
+  }
 });
 
 const json = computed(() => {
-  return JSON.stringify(
-    jsonData.value,
-    false,
-    4
-  );
+  if (jsonData.value) {
+    return JSON.stringify(
+      jsonData.value,
+      false,
+      parseInt(spacing.value)
+    );
+  } else {
+    return '';
+  }
+});
+
+const showTooltip = ref(false);
+
+const tooltipClass = computed(() => {
+  if (showTooltip.value) {
+    return 'tooltip show';
+  } else {
+    return 'tooltip';
+  }
+});
+
+function copyCode() {
+  navigator.clipboard.writeText(json.value);
+  showTooltip.value = true;
+  setTimeout(() => showTooltip.value = false, 3000);
+}
+
+
+onMounted(() => {
+  Prism.highlightAll(false);
 });
 </script>
 
@@ -53,14 +77,100 @@ const json = computed(() => {
   >
     <template #body>
       <div class="body">
-        <textarea v-model="rawJson" />
+        <div class="input-field">
+          <div class="heading">Raw JSON:</div>
 
-        <pre>
-          <code>
+          <textarea
+            v-model="rawJson"
+          />
+
+          <select v-model="spacing">
+            <option value="0">Compact</option>
+            <option value="2">2-space</option>
+            <option value="4">4-space</option>
+          </select>
+        </div>
+
+        <div class="output-field">
+          <div class="heading">Formatted:</div>
+
+          <button
+            v-if="json"
+            class="btn"
+            @click="copyCode"
+          >📋</button>
+
+          <div
+            :class="tooltipClass"
+          >Copied formatted JSON!</div>
+
+          <pre class="language-json">
+            <code>
 {{ json }}
-          </code>
-        </pre>
+            </code>
+          </pre>
+        </div>
       </div>
     </template>
   </ToolWindow>
 </template>
+
+<style scoped>
+.body {
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+}
+
+.body .input-field textarea {
+  width: 100%;
+  min-height: 100px;
+}
+
+.body .input-field .heading,
+.body .output-field .heading {
+  font-weight: 600;
+}
+
+.body .output-field {
+  position: relative;
+}
+
+.body .output-field pre {
+  padding: 1rem;
+  width: 100%;
+  overflow: auto;
+  border: 1px solid var(--color-border);
+  border-radius: 4px;
+}
+
+.body pre code {
+  font-size: 0.8rem;
+}
+
+.body .output-field .btn {
+  position: absolute;
+  top: 2rem;
+  right: 1rem;
+}
+
+.body .output-field .tooltip {
+  display: none;
+  position: absolute;
+  top: 4rem;
+  right: 1rem;
+  padding: 0.5rem;
+  text-align: center;
+  font-size: 0.8rem;
+  background-color: var(--color-background);
+  color: var(--color-text);
+  border: 1px solid var(--color-border);
+  border-radius: 4px;
+  opacity: 0.8;
+  z-index: 1;
+}
+
+.body .output-field .tooltip.show {
+  display: inline-block;
+}
+</style>
